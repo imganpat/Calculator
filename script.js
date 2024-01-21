c66858cff68a1e571c6321b128af62d3af4fb083
imganpat <coolganpat31@gmail.com>
2024-01-21 17:18:55 +0530

    made the equation section functional without reloading

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const result = document.querySelector(".result");
+const equation = document.querySelector(".equation");
 const buttons = document.querySelectorAll("button");
 const arr = Array.from(buttons);
 
 for (let i = 0; i < arr.length; i++) {
   arr[i].addEventListener("click", () => {
-    if (arr[i].innerText === "AC")
+    if (arr[i].innerText === "AC") {
+      equation.innerText = "";
       result.innerText = "";
+    }
     else if (arr[i].innerText === "Del")
       result.innerText = result.innerText.substring(0, result.innerText.length - 1);
     else if (arr[i].innerText === "=" && result.innerHTML != "") {
+      equation.innerText = result.innerHTML;
       result.innerText = eval(result.innerText);
     }
     else
       result.innerText += arr[i].innerText;
   });
 }
+
+
+
 
 
